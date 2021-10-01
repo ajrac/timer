893201026903fe09a6e2cd2443d330f740f3fdb1
Anthony Racioppo <ajrac@udel.edu>
2021-09-30 20:55:07 -0400

    Updates

---
+++ static/js/main.e193ab2d.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonptimer=this.webpackJsonptimer||[]).push([[0],{10:function(t,n,e){"use strict";e.r(n);var c=e(0),i=e.n(c),o=e(3),r=e.n(o),s=(e(8),function(t){t&&t instanceof Function&&e.e(3).then(e.bind(null,11)).then((function(n){var e=n.getCLS,c=n.getFID,i=n.getFCP,o=n.getLCP,r=n.getTTFB;e(t),c(t),i(t),o(t),r(t)}))}),u=e(1);var a=function(){return Object(u.jsx)("section",{})};r.a.render(Object(u.jsx)(i.a.StrictMode,{children:Object(u.jsx)(a,{})}),document.getElementById("root")),s()},8:function(t,n,e){}},[[10,1,2]]]);
+//# sourceMappingURL=main.e193ab2d.chunk.js.map
